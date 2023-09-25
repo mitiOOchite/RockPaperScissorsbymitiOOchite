ec59a3b6837fc348c513a94ccc10e257c6e99052
mitiOOchite <dimitartsvetkov26@protonmail.com>
2023-09-25 17:58:04 +0300

    Yeahh boy

--- computer_moves.py
+++ computer_moves.py
@@ -1,15 +1,13 @@
 import random
+import rock_paper_scissors_var
 def computer_attribute():
-    computer_rock = 'Rock'
-    computer_paper = 'Paper'
-    computer_scissors = 'Scissors'
     computer_random_number = random.randint(1, 3)
     computer_move = ''
     if computer_random_number == 1:
-        computer_move = computer_rock
+        computer_move = rock_paper_scissors_var.ROCK
     elif computer_random_number == 2:
-        computer_move = computer_paper
+        computer_move = rock_paper_scissors_var.PAPER
     elif computer_random_number == 3:
-        computer_move = computer_scissors
+        computer_move = rock_paper_scissors_var.SCISSORS
     print(f'The computer chose: {computer_move}')
     return computer_move
--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,20 +1,17 @@
 from player_moves import player_attribute
 from computer_moves import computer_attribute
+import rock_paper_scissors_var
 player_move = player_attribute()
 computer_move = computer_attribute()
-ROCK = 'Rock'
-SCISSORS = 'Scissors'
-PAPER = 'Paper'
-
-if player_move == computer_move:
-    win_or_lose = 'Draw!'
-elif computer_move == ROCK and player_move == SCISSORS:
-    win_or_lose = 'You Lose!'
-elif computer_move == PAPER and player_move == ROCK:
-    win_or_lose = 'You Lose!'
-elif computer_move == SCISSORS and player_move == PAPER:
-    win_or_lose = 'You Lose!'
-else:
-    win_or_lose = 'You Win!'
-print(win_or_lose)
-
+def rock_paper_scissors():
+    if player_move == computer_move:
+        win_or_lose = rock_paper_scissors_var.DRAW
+    elif computer_move == rock_paper_scissors_var.ROCK and player_move == rock_paper_scissors_var.SCISSORS:
+        win_or_lose = rock_paper_scissors_var.LOSE
+    elif computer_move == rock_paper_scissors_var.PAPER and player_move == rock_paper_scissors_var.ROCK:
+        win_or_lose = rock_paper_scissors_var.LOSE
+    elif computer_move == rock_paper_scissors_var.SCISSORS and player_move == rock_paper_scissors_var.PAPER:
+        win_or_lose = rock_paper_scissors_var.LOSE
+    else:
+        win_or_lose = rock_paper_scissors_var.WIN
+    print(win_or_lose)
