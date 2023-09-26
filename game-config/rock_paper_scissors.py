62e173c389005574f51a78be2dba6859f6b809b2
mitiOOchite <dimitartsvetkov26@protonmail.com>
2023-09-26 15:36:27 +0300

    Create directory for game configuration

--- game-config/rock_paper_scissors.py
+++ game-config/rock_paper_scissors.py
@@ -1,17 +1,45 @@
 from player_moves import player_attribute
 from computer_moves import computer_attribute
 import rock_paper_scissors_var
-player_move = player_attribute()
-computer_move = computer_attribute()
 def rock_paper_scissors():
-    if player_move == computer_move:
-        win_or_lose = rock_paper_scissors_var.DRAW
-    elif computer_move == rock_paper_scissors_var.ROCK and player_move == rock_paper_scissors_var.SCISSORS:
-        win_or_lose = rock_paper_scissors_var.LOSE
-    elif computer_move == rock_paper_scissors_var.PAPER and player_move == rock_paper_scissors_var.ROCK:
-        win_or_lose = rock_paper_scissors_var.LOSE
-    elif computer_move == rock_paper_scissors_var.SCISSORS and player_move == rock_paper_scissors_var.PAPER:
-        win_or_lose = rock_paper_scissors_var.LOSE
-    else:
-        win_or_lose = rock_paper_scissors_var.WIN
-    print(win_or_lose)
+    rock_paper_scissors_var.NUMBER_OF_GAMES
+    rock_paper_scissors_var.computer_win_count = 0
+    rock_paper_scissors_var.player_win_count = 0
+    rock_paper_scissors_var.win_count = 0
+    i = rock_paper_scissors_var.NUMBER_OF_GAMES
+    while i <= rock_paper_scissors_var.NUMBER_OF_GAMES:
+        if rock_paper_scissors_var.NUMBER_OF_GAMES == 3 and rock_paper_scissors_var.player_win_count == 2:
+            winner = rock_paper_scissors_var.USER
+            rock_paper_scissors_var.win_count = rock_paper_scissors_var.player_win_count
+            break
+        elif rock_paper_scissors_var.NUMBER_OF_GAMES == 3 and rock_paper_scissors_var.computer_win_count == 2:
+            winner = "Computer"
+            rock_paper_scissors_var.win_count = rock_paper_scissors_var.computer_win_count
+            break
+        if i == 0:
+            break
+        player_move = player_attribute()
+        computer_move = computer_attribute()
+        if player_move == computer_move:
+            win_or_lose = rock_paper_scissors_var.DRAW
+            i -= 0
+            print(win_or_lose)
+            continue
+        elif computer_move == rock_paper_scissors_var.ROCK and player_move == rock_paper_scissors_var.SCISSORS:
+            win_or_lose = rock_paper_scissors_var.LOSE
+            rock_paper_scissors_var.computer_win_count += 1
+            i -= 1
+        elif computer_move == rock_paper_scissors_var.PAPER and player_move == rock_paper_scissors_var.ROCK:
+            win_or_lose = rock_paper_scissors_var.LOSE
+            rock_paper_scissors_var.computer_win_count += 1
+            i -= 1
+        elif computer_move == rock_paper_scissors_var.SCISSORS and player_move == rock_paper_scissors_var.PAPER:
+            win_or_lose = rock_paper_scissors_var.LOSE
+            rock_paper_scissors_var.computer_win_count += 1
+            i -= 1
+        else:
+            win_or_lose = rock_paper_scissors_var.WIN
+            rock_paper_scissors_var.player_win_count += 1
+            i -= 1
+        print(win_or_lose)
+    print(f'The winner is {winner} with {rock_paper_scissors_var.win_count}')
--- game-config/rock_paper_scissors_var.py
+++ game-config/rock_paper_scissors_var.py
@@ -1,6 +1,16 @@
 ROCK = 'Rock'
 SCISSORS = 'Scissors'
 PAPER = 'Paper'
-DRAW = 'Draw!'
-WIN = 'You Win!'
-LOSE = 'You Lose!'
+DRAW = 'This round was a draw, it won\'t be counted!'
+WIN = 'You win this round!'
+LOSE = 'You lose this round!'
+USER = input('Please choose a username: ')
+NUMBER_OF_GAMES = int(input('You are able to choose to play [3], [5], [7] or [9] games: '))
+computer_win_count = 0 # win count stores the number of games won by the computer
+player_win_count = 0 # stored the number of games won by the player
+win_count = 0
+#game variations for a win count as follows:
+#best of 3 = 2 wins out of 3 games are considered a win(2 of 3)
+#best of 5 = 3 wins out of 5 games are considered a win(3 of 5)
+#best of 7 = 4 wins out of 7 games ar econsidered a win(4 of 7)
+#best of 9 = 5 wins out of 7 games are considered a win(5 of 9)
